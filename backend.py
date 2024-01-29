1b71972a7cf547c80e3bbd146bf781ffb9d98b4a
GaneshMunna4566 <71213041+GaneshMunna4566@users.noreply.github.com>
2024-01-29 02:11:53 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,12 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/pap', methods=['POST'])
-def den():
-    # Accessing request data
-    data = request.json
-    # Process the data (for demonstration, just echoing back)
-    return jsonify(data)
 
 @app.route('/', methods=['GET'])
 def hello():
